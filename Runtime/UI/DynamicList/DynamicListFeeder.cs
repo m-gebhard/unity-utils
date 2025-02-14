0611d2818018db5c2b55e63b69b1732cd2c341e6
Marius Gebhard <marius.wspi@gmail.com>
2025-02-14 16:40:40 +0100

    add dynamic list classes

---
+++ Runtime/UI/DynamicList/DynamicListFeeder.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Abstract class for feeding dynamic list items.
+/// </summary>
+public abstract class DynamicListFeeder : MonoBehaviour
+{
+    /// <summary>
+    /// Gets the list of dynamic list items.
+    /// </summary>
+    /// <returns>A list of dynamic list items.</returns>
+    public abstract List<DynamicListItem> GetItems();
+}
