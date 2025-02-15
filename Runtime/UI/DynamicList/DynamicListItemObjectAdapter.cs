9807dad88e3240727eda1c5b966e6ec37b34d560
Marius Gebhard <marius.wspi@gmail.com>
2025-02-15 15:07:49 +0100

    add namespace

--- Runtime/UI/DynamicList/DynamicListItemObjectAdapter.cs
+++ Runtime/UI/DynamicList/DynamicListItemObjectAdapter.cs
@@ -1,28 +1,31 @@
 using UnityEngine;
 
-/// <summary>
-/// Abstract class for adapting dynamic list items in the UI.
-/// Has to be placed on the prefab of the list item.
-/// Is responsible for setting the UI of the list item.
-/// </summary>
-public abstract class DynamicListItemObjectAdapter : MonoBehaviour
+namespace UnityUtils.UI
 {
     /// <summary>
-    /// Gets the RectTransform component of the list item.
+    /// Abstract class for adapting dynamic list items in the UI.
+    /// Has to be placed on the prefab of the list item.
+    /// Is responsible for setting the UI of the list item.
     /// </summary>
-    public RectTransform Rect { get; private set; }
-
-    /// <summary>
-    /// Sets the data for the dynamic list item.
-    /// </summary>
-    /// <param name="item">The data to set for the list item.</param>
-    public abstract void SetData(DynamicListItem item);
-
-    /// <summary>
-    /// Called when the script instance is being loaded.
-    /// </summary>
-    private void Awake()
+    public abstract class DynamicListItemObjectAdapter : MonoBehaviour
     {
-        Rect = GetComponent<RectTransform>();
+        /// <summary>
+        /// Gets the RectTransform component of the list item.
+        /// </summary>
+        public RectTransform Rect { get; private set; }
+
+        /// <summary>
+        /// Sets the data for the dynamic list item.
+        /// </summary>
+        /// <param name="item">The data to set for the list item.</param>
+        public abstract void SetData(DynamicListItem item);
+
+        /// <summary>
+        /// Called when the script instance is being loaded.
+        /// </summary>
+        private void Awake()
+        {
+            Rect = GetComponent<RectTransform>();
+        }
     }
 }
