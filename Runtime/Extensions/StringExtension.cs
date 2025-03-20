7c3c811b91643f51b57da47c589b89bb61a700a1
Marius Gebhard <marius.wspi@gmail.com>
2025-03-20 22:31:51 +0100

    Add string sanitize extension

--- Runtime/Extensions/StringExtension.cs
+++ Runtime/Extensions/StringExtension.cs
@@ -19,5 +19,24 @@ namespace UnityUtils.Extensions
             return Regex.Replace(text, Regex.Escape(swapA) + "|" + Regex.Escape(swapB),
                 m => m.Value == swapA ? swapB : swapA);
         }
+
+        /// <summary>
+        /// Sanitizes the input string by replacing spaces with underscores and removing special characters.
+        /// </summary>
+        /// <param name="text">The input string to be sanitized.</param>
+        /// <param name="allowedPattern">
+        /// A regex pattern defining the set of allowed characters.
+        /// Defaults to "A-Za-z0-9_", which allows alphanumeric characters and underscores.
+        /// </param>
+        /// <returns>A sanitized string with spaces replaced by underscores and disallowed characters removed.</returns>
+        public static string Sanitize(this string text, string allowedPattern = "A-Za-z0-9_")
+        {
+            if (string.IsNullOrEmpty(text)) return string.Empty;
+
+            text = text.Replace(" ", "_");
+            text = System.Text.RegularExpressions.Regex.Replace(text, @$"[^{allowedPattern}]", "");
+
+            return text;
+        }
     }
 }
