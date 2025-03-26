a8345a5a06c5254e55a778325619f823fcdfd5c9
Marius Gebhard <marius.wspi@gmail.com>
2025-03-26 16:05:02 +0100

    optimize decisiontree update method

--- Runtime/DecisionTree/DecisionTree.cs
+++ Runtime/DecisionTree/DecisionTree.cs
@@ -27,39 +27,10 @@ namespace UnityUtils.DecisionTree
         /// <returns>The status of the process.</returns>
         public override Status Process(float deltaTime)
         {
-            if (Children.Count == 0)
-                return Status.Failure;
+            Children[CurrentChildIndex].Process(deltaTime);
+            CurrentChildIndex = (CurrentChildIndex + 1) % Children.Count;
 
-            bool loopedOnce = false;
-
-            while (true)
-            {
-                Status status = Children[CurrentChildIndex].Process(deltaTime);
-
-                if (status == Status.Failure)
-                {
-                    CurrentChildIndex = 0;
-                    return status;
-                }
-
-                if (status == Status.Running)
-                {
-                    return Status.Running;
-                }
-
-                CurrentChildIndex++;
-                if (CurrentChildIndex >= Children.Count)
-                {
-                    CurrentChildIndex = 0;
-                    loopedOnce = true;
-                }
-
-                if (loopedOnce)
-                    break;
-            }
-
-            Reset();
-            return Status.Success;
+            return Status.Running;
         }
 
         /// <summary>
