314fb3c3bdfc0b82e94511298c34fb00f5d4f539
Marius Gebhard <marius.wspi@gmail.com>
2025-04-26 19:36:02 +0200

    add lerp helper

--- Runtime/Data/FadeTransitions.cs
+++ Runtime/Data/FadeTransitions.cs
@@ -2,107 +2,110 @@ using System;
 using System.Collections;
 using UnityEngine;
 
-/// <summary>
-/// Utility class for different transitions of various types.
-/// </summary>
-public class FadeTransitions : MonoBehaviour
+namespace UnityUtils.Data
 {
     /// <summary>
-    /// Generic coroutine to fade a value over time.
+    /// Utility class for different transitions of various types.
     /// </summary>
-    /// <typeparam name="T">The type of the value to fade.</typeparam>
-    /// <param name="getter">Function to get the current value.</param>
-    /// <param name="setter">Action to set the new value.</param>
-    /// <param name="targetValue">The target value to fade to.</param>
-    /// <param name="lerpFunc">Function to interpolate between values.</param>
-    /// <param name="duration">The duration of the fade in seconds.</param>
-    /// <returns>An IEnumerator for use in a coroutine.</returns>
-    public static IEnumerator FadeValue<T>(
-        Func<T> getter,
-        Action<T> setter,
-        T targetValue,
-        Func<T, T, float, T> lerpFunc,
-        float duration = 1f
-    )
+    public class FadeTransitions : MonoBehaviour
     {
-        float elapsed = 0f;
-        T startValue = getter();
-
-        while (elapsed < duration)
+        /// <summary>
+        /// Generic coroutine to fade a value over time.
+        /// </summary>
+        /// <typeparam name="T">The type of the value to fade.</typeparam>
+        /// <param name="getter">Function to get the current value.</param>
+        /// <param name="setter">Action to set the new value.</param>
+        /// <param name="targetValue">The target value to fade to.</param>
+        /// <param name="lerpFunc">Function to interpolate between values.</param>
+        /// <param name="duration">The duration of the fade in seconds.</param>
+        /// <returns>An IEnumerator for use in a coroutine.</returns>
+        public static IEnumerator FadeValue<T>(
+            Func<T> getter,
+            Action<T> setter,
+            T targetValue,
+            Func<T, T, float, T> lerpFunc,
+            float duration = 1f
+        )
         {
-            elapsed += Time.deltaTime;
-            float t = elapsed / duration;
+            float elapsed = 0f;
+            T startValue = getter();
 
-            T newValue = lerpFunc(startValue, targetValue, t);
-            setter(newValue);
+            while (elapsed < duration)
+            {
+                elapsed += Time.deltaTime;
+                float t = elapsed / duration;
 
-            yield return null;
+                T newValue = lerpFunc(startValue, targetValue, t);
+                setter(newValue);
+
+                yield return null;
+            }
+
+            setter(targetValue);
         }
 
-        setter(targetValue);
-    }
+        /// <summary>
+        /// Coroutine to fade a material's color property over time.
+        /// </summary>
+        /// <param name="targetMaterial">The material to modify.</param>
+        /// <param name="targetColor">The target color to fade to.</param>
+        /// <param name="fieldName">The name of the color property to modify (default is "_EmissionColor").</param>
+        /// <param name="duration">The duration of the fade in seconds.</param>
+        /// <returns>An IEnumerator for use in a coroutine.</returns>
+        public static IEnumerator FadeMaterialColor(
+            Material targetMaterial,
+            Color targetColor,
+            string fieldName = "_EmissionColor",
+            float duration = 1f
+        )
+        {
+            int propertyId = Shader.PropertyToID(fieldName);
 
-    /// <summary>
-    /// Coroutine to fade a material's color property over time.
-    /// </summary>
-    /// <param name="targetMaterial">The material to modify.</param>
-    /// <param name="targetColor">The target color to fade to.</param>
-    /// <param name="fieldName">The name of the color property to modify (default is "_EmissionColor").</param>
-    /// <param name="duration">The duration of the fade in seconds.</param>
-    /// <returns>An IEnumerator for use in a coroutine.</returns>
-    public static IEnumerator FadeMaterialColor(
-        Material targetMaterial,
-        Color targetColor,
-        string fieldName = "_EmissionColor",
-        float duration = 1f
-    )
-    {
-        int propertyId = Shader.PropertyToID(fieldName);
+            return FadeValue(
+                () => targetMaterial.GetColor(propertyId),
+                value => targetMaterial.SetColor(propertyId, value),
+                targetColor,
+                Color.Lerp,
+                duration
+            );
+        }
 
-        return FadeValue(
-            () => targetMaterial.GetColor(propertyId),
-            value => targetMaterial.SetColor(propertyId, value),
+        /// <summary>
+        /// Coroutine to fade a light's intensity over time.
+        /// </summary>
+        /// <param name="targetLight">The light to modify.</param>
+        /// <param name="targetIntensity">The target intensity to fade to.</param>
+        /// <param name="duration">The duration of the fade in seconds.</param>
+        /// <returns>An IEnumerator for use in a coroutine.</returns>
+        public static IEnumerator FadeLightIntensity(
+            Light targetLight,
+            float targetIntensity,
+            float duration = 1f
+        ) => FadeValue(
+            () => targetLight.intensity,
+            value => targetLight.intensity = value,
+            targetIntensity,
+            Mathf.Lerp,
+            duration
+        );
+
+        /// <summary>
+        /// Coroutine to fade a light's color over time.
+        /// </summary>
+        /// <param name="targetLight">The light to modify.</param>
+        /// <param name="targetColor">The target color to fade to.</param>
+        /// <param name="duration">The duration of the fade in seconds.</param>
+        /// <returns>An IEnumerator for use in a coroutine.</returns>
+        public static IEnumerator FadeLightColor(
+            Light targetLight,
+            Color targetColor,
+            float duration = 1f
+        ) => FadeValue(
+            () => targetLight.color,
+            value => targetLight.color = value,
             targetColor,
             Color.Lerp,
             duration
         );
     }
-
-    /// <summary>
-    /// Coroutine to fade a light's intensity over time.
-    /// </summary>
-    /// <param name="targetLight">The light to modify.</param>
-    /// <param name="targetIntensity">The target intensity to fade to.</param>
-    /// <param name="duration">The duration of the fade in seconds.</param>
-    /// <returns>An IEnumerator for use in a coroutine.</returns>
-    public static IEnumerator FadeLightIntensity(
-        Light targetLight,
-        float targetIntensity,
-        float duration = 1f
-    ) => FadeValue(
-        () => targetLight.intensity,
-        value => targetLight.intensity = value,
-        targetIntensity,
-        Mathf.Lerp,
-        duration
-    );
-
-    /// <summary>
-    /// Coroutine to fade a light's color over time.
-    /// </summary>
-    /// <param name="targetLight">The light to modify.</param>
-    /// <param name="targetColor">The target color to fade to.</param>
-    /// <param name="duration">The duration of the fade in seconds.</param>
-    /// <returns>An IEnumerator for use in a coroutine.</returns>
-    public static IEnumerator FadeLightColor(
-        Light targetLight,
-        Color targetColor,
-        float duration = 1f
-    ) => FadeValue(
-        () => targetLight.color,
-        value => targetLight.color = value,
-        targetColor,
-        Color.Lerp,
-        duration
-    );
 }
